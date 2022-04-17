e246d153f71702554b051b1b198b5f93e67196aa
HongJinHyeok <redmax45@naver.com>
2022-04-17 16:34:53 +0900

    ♻️ reinit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  plugins: ['prettier'],
-  extends: ['prettier', 'plugin:prettier/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
-    allowImportExportEverywhere: true,
   },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,11 @@
-'use strict';
-Object.defineProperty(exports, 'commonjs', { value: true });
-
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testEnvironment: "node",
+  rootDir: "src",
   transform: {
-    '^.+\\.(js|jsx)?$': 'babel-jest',
+    "^.+\\.(t|j)s$": "ts-jest",
   },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
-  },
-  testMatch: [
-    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
-    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
-  ],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  testPathIgnorePatterns: ['lib/', 'node_modules/'],
-  testEnvironment: 'node',
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$",
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}", "!src/**/*.d.ts"],
 };
