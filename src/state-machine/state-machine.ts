6a732f168ffdce867d4f86503d09063ff30d175f
HongJinHyeok <redmax45@naver.com>
2022-04-18 22:53:22 +0900

    ⚡️ state-machine

--- src/state-machine/state-machine.ts
+++ src/state-machine/state-machine.ts
@@ -7,6 +7,26 @@ import {
 
 type Event = 'BeforeCallSignal' | 'CallSucceed' | 'CallFailed';
 
-// type Matcher = {
-//   on: (event: Event, fn:())
-// }
+type Matcher = {
+  on: (event: Event, fn: () => CircuitBreakerState) => Matcher;
+  transition: () => CircuitBreakerStateMachine;
+};
+
+class CircuitBreakerStateMachine {
+  constructor(
+    readonly currentState: CircuitBreakerState,
+    private readonly isThresholdReached: (fails: number) => boolean,
+    private readonly isTimeoutReached: (openCircuit: OpenCircuit) => boolean,
+  ) {}
+
+  private matched = (state: CircuitBreakerState) => ({
+    on: () => this.matched(state),
+    transition: () => this.transitionTo(state),
+  });
+
+  private match: (matchEvent: Event) => Matcher = (matchEvent: Event) => ({
+    on: (event: Event, fn: () => CircuitBreakerState) =>
+      event === matchEvent ? this.matched(fn()) : this.match(matchEvent),
+    transition: () => this,
+  });
+}
